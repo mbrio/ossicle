8bbd9b2897fd3b57d9b59af179895d69f9a83dcf
k33g <ph.charriere@gmail.com>
2011-08-21 20:00:07 +0200

    ossicle model and models

--- js/ossicle.js
+++ js/ossicle.js
@@ -6,7 +6,96 @@
  * To change this template use File | Settings | File Templates.
  */
 (function(){
+
+    /*------------------------------------------------------------*/
+    /*                       STORAGE                              */
+    /*------------------------------------------------------------*/
+
+    var WareHouse = {
+
+        getLocalStorage :function (kindOfStore, storageName) {
+            return {
+
+                storageName : storageName,
+
+
+                isAvailable : function () {
+                    try {
+                        kindOfStore.setItem("testKey", "testValue");
+                        kindOfStore.removeItem("testKey");
+                    } catch (err) {
+                        return false; /*not available*/
+                    }
+                    return true;
+                },
+
+                get : function (key) {
+                    var obj = JSON.parse(kindOfStore.getItem(this.storageName + '|' + key));
+                    if (obj) {obj.key = key; return obj; } else { return null; }
+                },
+
+                remove : function (keyOrObject) {
+                    var key = this.storageName + '|' + (typeof keyOrObject === 'string' ? keyOrObject : keyOrObject.key);
+                    /*TODO: have to verify if exists before delete*/
+                    kindOfStore.removeItem(key);
+                    return key;
+                },
+
+                save : function (obj) {
+                    //var id = this.storageName + '|' + (obj.key || cyste.guidGenerator());
+                    var id = this.storageName + '|' + obj.key;
+                    delete obj.key;
+                    //try {
+                        kindOfStore.setItem(id, JSON.stringify(obj));
+
+                        return obj.key = id.split('|')[1];
+
+                    //} catch (err) { throw (err); }
+                },
+
+                all : function () {
+                    var results = [], i, store = kindOfStore, l = store.length, id, key, baseName, obj;
+                    for (i = 0; i < l; i += 1) {
+                        id = store.key(i);
+                        baseName = id.split('|')[0];
+                        key = id.split('|').slice(1).join("|");
+                        if (baseName === this.storageName) {
+                            obj = JSON.parse(kindOfStore.getItem(id));
+                            obj.key = key;
+                            results.push(obj);
+                        }
+                    }
+                    return results;
+                },
+
+                drop : function () {
+                    var that = this;
+
+                    this.all(function (r) {
+                        var m;
+                        for (m in r) {
+                            if (r.hasOwnProperty(m)) {
+                                that.remove(r[m].key, null);
+                            }
+                        }
+                    });
+                }
+            };
+        },
+
+        getStorage : function (baseName) {
+            var storage = this.getLocalStorage(window.localStorage, baseName);
+            if (!storage.isAvailable()) { storage = null; }
+            return storage;
+        }
+
+    }
+
+
+
+
     Ossicle = this.Ossicle = {};
+    
     Ossicle.Class = {
         extend : function(class_def) {
             var m, k = Object.create(this);
